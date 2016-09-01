8685991a63e0b8a8b59fd9d77e5735b65dd4099f
virmerson <virmerson@hightechcursos.com.br>
2016-09-01 11:22:15 -0400

    Server Node parte 2

--- apiweb/server.js
+++ apiweb/server.js
@@ -1,28 +1,22 @@
-
 var http = require("http");
-var http = require("url");
+var url = require("url");
 
 var server = http.createServer(function (request, response){
     //O que vai no HEAD do cabeçalho HTTP
     response.writeHead(200, {"content-type":"text/html"});
+    response.write("<h1> Dados da query String </h1>");
     
-    
-    if(request.url=="/"){
-        
-        response.write("<h1>Página inicial</h1>");
-        
-    }else if (request.url=="/bemvindo"){
-        
-        response.write("<h1> Bem Vindo :D </h1");
-    }else {
-         
-        response.write("<h1> Página Não encontrada </h1>");
+    //?nome=jao&email=jao@htcursos.com&fone=889899
+    var  result = url.parse(request.url, true);
+    //                    K      v            k             V                k      v
+    //result.query = [ {"nome","jao"} ,   {"email":"jao@htcursos.com"}  , {"fone","6565"} ]
+    //                        0                     1                            2
+    for (var key in result.query){
+        response.write("<h2>"+ key + ": "+ result.query[key]+ "</h2>");
     }
-  
     response.end();
     
 });
-
 
 var servidorLigou=function(){
     console.log("Servidor Hello rodando!");
